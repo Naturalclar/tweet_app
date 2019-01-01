c4774894e096da17ee067330b1cf9ce2f23de95e
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-01-01 20:49:30 +0900

    :sparkles: add flash notification for posting and deleting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,12 @@ class PostsController < ApplicationController
     @posts = Post.all.order(created_at: :desc)
   end
   def new
+    @post = Post.new
   end
   def create
     @post = Post.new(content: params[:content])
     if @post.save
+      flash[:notice] = "投稿を作成しました"
       redirect_to('/posts/index')
     else
       render("posts/new")
@@ -18,6 +20,7 @@ class PostsController < ApplicationController
   def delete
     @post = Post.find_by(id: params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました"
     redirect_to('/posts/index')
   end
   def update
