24517fb62837f14c266e23574d2d65ab2ed2b79a
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-01-01 15:26:59 +0900

    :sparkles: add edit function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/new'
+  get 'posts/:id/edit' => 'posts#edit'
   get 'posts/:id' => 'posts#show'
+  post 'posts/:id/update' => 'posts#update'
   post 'posts/create'
   get '/' => 'home#top'
   get 'about' => 'home#about'
