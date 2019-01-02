b419d15bc892a9a79f5f7ff38be9420ab3f5b025
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-01-02 16:32:20 +0900

    :sparkles: add user list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # users
+  get 'users/index'
+  # posts
   get 'posts/index'
   get 'posts/new'
   get 'posts/:id/edit' => 'posts#edit'
@@ -6,6 +9,7 @@ Rails.application.routes.draw do
   post 'posts/:id/delete' => 'posts#delete'
   post 'posts/:id/update' => 'posts#update'
   post 'posts/create'
+  # home
   get '/' => 'home#top'
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
