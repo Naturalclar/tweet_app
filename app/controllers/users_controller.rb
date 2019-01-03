9921f0a6fe43d18ebd1bdafd4c5ea8c38af3637c
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-01-03 20:29:02 +0900

    :sparkles: add feature to add icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(name:params[:name],email:params[:email])
+    @user = User.new(
+      name:params[:name],
+      email:params[:email],
+      image_name: "default_icon.svg"
+      )
     if @user.save
       flash[:notice] = "New user created"
       redirect_to('/posts/index')
@@ -24,9 +28,14 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
     if @user.save
       flash[:notice] = "Successfully updated user info"
-      redirect_to("/users/#{@user.id}/edit")
+      redirect_to("/users/#{@user.id}")
     else
       render("users/edit")
     end
